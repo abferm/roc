50843b98e9bbaec8e59e0dac30d6d7c69d161b6d
Alex Ferm <aferm@petropower.com>
2017-09-28 00:59:38 -0500

    Added Stringer interface to TLP

--- tlp.go
+++ tlp.go
@@ -1,7 +1,13 @@
 package roc
 
+import "fmt"
+
 type TLP struct {
 	PointType   uint8
 	LogicNumber uint8
 	Parameter   uint8
+}
+
+func (tlp TLP) String() string {
+	return fmt.Sprintf("%d.%d.%d", tlp.PointType, tlp.LogicNumber, tlp.Parameter)
 }
